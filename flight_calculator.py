53d2aeab072a05ea2e0b918b51cfa0b9c841c276
Pawel Piekarski <piekpk@farmingdale.edu>
2026-08-31 01:18:29 -0400

    added testing to flight calculator

--- flight_calculator.py
+++ flight_calculator.py
@@ -46,3 +46,22 @@ def flight_time_table(max_weight_grams, step_grams):
         current_weight += step_grams
 
     return table
+
+
+from flight_calculator import calculate_flight_time, flight_time_table
+
+def test_calculate_flight_time_zero():
+    assert calculate_flight_time(0) == 180.0
+
+def test_calculate_flight_time_500():
+    assert calculate_flight_time(500) == 130.0
+
+def test_flight_time_table():
+    assert flight_time_table(100, 50) == [(0, 180.0), (50, 175.0), (100, 170.0)]
+
+def test_negative_weight_raises():
+    try:
+        calculate_flight_time(-1)
+        assert False
+    except ValueError:
+        pass
